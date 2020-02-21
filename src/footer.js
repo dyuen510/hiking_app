d825a6042e0296888cb4657b2460095abb301a91
David <davidyuen0305@gmail.com>
2020-02-20 16:32:22 -0800

    added logos and links to connect at the footer.

--- src/footer.js
+++ src/footer.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css'
+import Social from './social.js';
 
 class Foot extends Component{
     render(){
         return (
             <div className = 'footer'>
+                < Social/>
                 <h2>@copyright</h2>
             </div>
             
---
+++ src/social.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css'
+import logo from './images/linkedin.png';
+import logo2 from './images/github.png';
+import logo3 from './images/instagram.png';
+
+class Social extends Component{
+    render(){
+        return (
+            <div className = 'socialMedia'>
+                <h3>Connect With Me</h3>
+                <a href = 'https://www.linkedin.com/in/david-yuen-549bb3b5/'><img src = {logo} /></a>
+                <a href = 'https://github.com/dyuen510'><img src = {logo2} /></a> 
+                <a href = 'https://www.instagram.com/dave.yuen'><img src = {logo3} width ='70px'/></a> 
+            </div>
+        )
+    }
+}
+
+export default Social
